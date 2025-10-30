38f2113f9130d1414ba198ac08bdaabf032ae4a1
sungjin <ksj48500@naver.com>
2025-10-30 12:04:44 +0900

    first commit

---
+++ jwbook2/src/main/java/com/jwbook2/jwbook2/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jwbook2.jwbook2.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	  http
+	   	  .authorizeHttpRequests(auth -> auth
+	   			  .requestMatchers("/").permitAll()
+	   			  .anyRequest().authenticated()
+	   			  );
+	  return http.build();
+  }
+}
